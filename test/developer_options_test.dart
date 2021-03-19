3b13298013062d8424443e27da5db33c1dc45d95
Vojtěch Pšenák <psenak.vojtech@gmail.com>
2021-03-20 00:24:58 +0100

    remove prints

--- test/developer_options_test.dart
+++ test/developer_options_test.dart
@@ -17,57 +17,63 @@ void main() {
       flutter_test.TestWidgetsFlutterBinding.ensureInitialized();
 
       DeveloperOptions developerOptions = new DeveloperOptions();
-      print("awaiting");
+
       await developerOptions.init();
-      print("successfully awaited");
+
       expect(developerOptions.isEnabled, false);
     });
 
     test('load DeveloperOptions and verify if enabled', () async {
-      print("got model");
       SharedPreferences.setMockInitialValues({
         "PKG_DEVELOPER_OPTIONS_KEY_DEVELOPER_OPTIONS": mockdata.toString(),
       });
-      print("got mock values");
-      flutter_test.TestWidgetsFlutterBinding.ensureInitialized();
 
+      flutter_test.TestWidgetsFlutterBinding.ensureInitialized();
       DeveloperOptions developerOptions = new DeveloperOptions();
-      print("awaiting");
+
       await developerOptions.init();
-      print("successfully awaited");
+
       expect(developerOptions.isEnabled, true);
     });
 
     test('load DeveloperOptions and verify retrieved values', () async {
-      print("got model");
       SharedPreferences.setMockInitialValues({
         "PKG_DEVELOPER_OPTIONS_KEY_DEVELOPER_OPTIONS": mockdata.toString(),
       });
-      print("got mock values");
+
       flutter_test.TestWidgetsFlutterBinding.ensureInitialized();
 
       DeveloperOptions developerOptions = new DeveloperOptions();
-      print("awaiting");
+
       await developerOptions.init();
-      print("successfully awaited");
+
       expect(developerOptions.getDeveloperString("testValue2"), "test");
     });
 
     test('load DeveloperOptions and verify retrieved options', () async {
-      print("got model");
       SharedPreferences.setMockInitialValues({
         "PKG_DEVELOPER_OPTIONS_KEY_DEVELOPER_OPTIONS": mockdata.toString(),
       });
-      print("got mock values");
+
       flutter_test.TestWidgetsFlutterBinding.ensureInitialized();
 
       DeveloperOptions developerOptions = new DeveloperOptions();
-      print("awaiting");
       await developerOptions.init();
-      print("successfully awaited");
+
       expect(developerOptions.getDeveloperOption("testValue1"), true);
     });
 
+    test('save DeveloperOptions and verify retrieved options', () async {
+      SharedPreferences.setMockInitialValues({
+        "PKG_DEVELOPER_OPTIONS_KEY_DEVELOPER_OPTIONS": mockdata.toString(),
+      });
 
+      flutter_test.TestWidgetsFlutterBinding.ensureInitialized();
+
+      DeveloperOptions developerOptions = new DeveloperOptions();
+      await developerOptions.init();
+
+      expect(developerOptions.getDeveloperOption("testValue1"), true);
+    });
   });
 }
