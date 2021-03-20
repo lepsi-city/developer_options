5835cb23b4a77a36a8d64e0383a1c6dad4bb99ae
Vojtěch Pšenák <psenak.vojtech@gmail.com>
2021-03-20 05:54:35 +0100

    Add basic example

---
+++ example/android/app/src/main/kotlin/com/example/developer_options_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.developer_options_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+import 'package:developer_options/developer_options.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool isDeveloper = false;
+  DeveloperOptions developerOptions = new DeveloperOptions();
+  String developerStatusText = "No";
+
+  void initState() async {
+    await developerOptions.init();
+    isDeveloper = developerOptions.getDeveloperStatus();
+    developerStatusText = isDeveloper ? "Yes" : "No";
+    super.initState();
+  }
+
+  void _setDeveloper() {
+    setState(() {
+      developerOptions.setDeveloperStatus(true);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Are you a developer?',
+            ),
+            Text(
+              developerStatusText,
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _setDeveloper,
+        tooltip: 'Be a developer',
+        child: Icon(Icons.hardware),
+      ),
+    );
+  }
+}
